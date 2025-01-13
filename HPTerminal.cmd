650392d13e4bebac99be306b4f897e91b0da2cfc
MartinHepperle <MartinHepperle@users.noreply.github.com>
2025-01-13 16:47:24 +0100

    Add files via upload

---
+++ HPTerminal.cmd
@@ -0,0 +1,11 @@
+@echo off
+rem ---------------------
+rem HP Terminal Emulator
+rem ---------------------
+
+rem Example calls
+rem java -jar HPTerminal.jar -sound 0
+rem java -jar HPTerminal.jar -sound 0 -debug 1 -logging 1
+rem java -jar HPTerminal.jar -port \\.\COM25 -speed 4800
+
+java -jar HPTerminal.jar
